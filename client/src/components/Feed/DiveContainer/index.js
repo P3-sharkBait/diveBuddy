b34589dbbbc27ae6486bd1e7656efb5c8d6dd4e3
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-07-07 18:46:31 -0500

    Added a button to add log

--- client/src/components/Feed/DiveContainer/index.js
+++ client/src/components/Feed/DiveContainer/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Dives from "../Dives";
 import { useQuery } from "@apollo/client";
 import { QUERY_USERS } from "../../../utils/queries";
+import { Link } from "react-router-dom";
 
 const DiveContainer = (props) => {
   const { loading, data } = useQuery(QUERY_USERS);
@@ -12,13 +13,15 @@ const DiveContainer = (props) => {
     <div className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div id="diveContainer">
-        {loading ? (
+          {loading ? (
             <div>Loading...</div>
           ) : (
-            <Dives
-              users={users}
-              title="Dive On In"
-            />
+            <>
+              <Link className="btn btn-md btn-info m-2" to="/">
+                Add Log
+              </Link>
+              <Dives users={users} title="Dive On In" />
+            </>
           )}
         </div>
       </div>
