615692a1a1f7935cb10cac9c0c19141ae703ae02
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-07-08 16:29:31 -0500

    setup test page for dashboard

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Dashboard = (props) => {
+  return (
+    <div>
+        The Dashboard
+    </div>
+  );
+};
+
+export default Dashboard;
