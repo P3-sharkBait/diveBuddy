5e408d83937dcc2402f03a307f53d2d917a238d6
Theo Ward <theoward9@gmail.com>
2022-10-06 20:19:08 -0500

    pausing on aws, log schema has been updated

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,31 @@
 const { AuthenticationError } = require("apollo-server-express");
-const { User, Log, Product, Order } = require("../models");
+const { User, Log, Product, Order, Image } = require("../models");
 const { signToken } = require("../utils/auth");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
 // const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccesKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_S3_BUCKET_NAME,
+    metadata: function (req, file, cb){
+      cb(null, {fieldname: file.fieldname});
+    },
+    contentType: function (req, file, cb){
+      cb(null, file.mimetype)
+    },
+    key: function (req, file, cb){
+      cb(null, `${Date.now().toString()}${file.originalname}`);
+    }
+  })
+});
+
 
 const resolvers = {
   Query: {
