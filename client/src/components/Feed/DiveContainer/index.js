1c2faefb0b7ea023c0e4adea45f4b66a54a7806b
Theo Ward <theoward9@gmail.com>
2022-10-20 19:50:49 -0500

    conditional render in myDives component, deleted dives and dive list

--- client/src/components/Feed/DiveContainer/index.js
+++ client/src/components/Feed/DiveContainer/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Dives from "../Dives";
+// import Dives from "../Dives";
 import { useQuery } from "@apollo/client";
 import { QUERY_USERS, QUERY_ME } from "../../../utils/queries";
 import { Link } from "react-router-dom";
@@ -8,57 +8,33 @@ import MyDives from "../MyDives";
 
 const DiveContainer = ({ filterState }) => {
   const username = Auth.getProfile().data.username;
-  console.log(filterState);
   const { loading, data } = useQuery(QUERY_USERS);
   const users = data?.users || [];
   const { loading: loadingMe, data: myData } = useQuery(QUERY_ME, {
     variables: { username: username },
   });
   const me = myData?.user || [];
-  console.log('--------------------');
-  console.log(me);
-  console.log(me.friends);
-  if (filterState === "All") {
-    return (
-      <main className="flex-column justify-center">
-        <div className="text-light mb-4 py-3 flex-column align-center">
-          <div className="flex-row justify-space-between-lg justify-center align-center">
-            <div id="diveContainer" className="container">
-              {loadingMe ? (
-                <div>Loading...</div>
-              ) : (
-                <>
-                  <Link className="btn btn-md btn-info m-2" to="/addLog">
-                    Add Log
-                  </Link>
-                  <Dives users={me.friends} me={me} title="Dive On In" />
-                </>
-              )}
-            </div>
+
+  return (
+    <main className="flex-column justify-center">
+      <div className="text-light mb-4 py-3 flex-column align-center">
+        <div className="flex-row justify-space-between-lg justify-center align-center">
+          <div id="diveContainer" className="container">
+            {loadingMe ? (
+              <div>Loading...</div>
+            ) : (
+              <>
+                <Link className="btn btn-md btn-info m-2" to="/addLog">
+                  Add Log
+                </Link>
+                <MyDives filterState={filterState} users={me.friends} me={me} title="Dive On In" />
+              </>
+            )}
           </div>
         </div>
-      </main>
-    );
-  } else {
-    return (
-        <div className="text-dark mb-4 py-3 flex-column align-center">
-          <div className="flex-column justify-space-between-lg justify-center align-center">
-            <div id="diveContainer" className="container">
-              {loadingMe ? (
-                <div>Loading...</div>
-              ) : (
-                <>
-                  <Link className="btn btn-md btn-info m-2" to="/addLog">
-                    Add Log
-                  </Link>
-                  <MyDives me={me} title="Dive On In" />
-                </>
-              )}
-            </div>
-          </div>
-        </div>
-    );
-  }
+      </div>
+    </main>
+  );
 };
 
 export default DiveContainer;
--- client/src/components/Feed/MyDives/index.js
+++ client/src/components/Feed/MyDives/index.js
@@ -1,7 +1,25 @@
 import React from "react";
 import MyDiveList from "../MyDiveList";
 
-const MyDives = ({ me, title, showTitle = true, showUsername = true }) => {
+const MyDives = ({ me, users, filterState, title, showTitle = true, showUsername = true }) => {
+  const divers = [];
+  const friendLogs = [];
+  divers.push(me);
+  users.forEach(element => {
+    divers.push(element)
+  });
+  divers.forEach(diver => {
+    console.log(diver.logs);
+    diver.logs.forEach(log => {
+      log['username'] = diver.username;
+    })
+    diver.logs.forEach(log => {
+      friendLogs.push(log);
+    })
+  })
+  //eventually need to fix createdAt in Log model to be truely chronological
+  let sortedLogs = friendLogs.sort(
+    (l1, l2) => (l1.diveNumber < l2.diveNumber) ? 1 : (l1.diveNumber > l2.diveNumber) ? -1 : 0);
   if (!me.logs.length) {
     return (
       <div className="dashContainer flex-column justify-space-between-lg  mb-4 py-3">
@@ -9,19 +27,33 @@ const MyDives = ({ me, title, showTitle = true, showUsername = true }) => {
       </div>
     );
   }
-  else return (
-    <div className="dashContainer container text-dark mb-4 py-3 flex-column align-center" style={{ height: "600px" }}>
-      {/* Create for each log???? */}
-      <div className="container flex-column justify-space-between-lg justify-center align-center">
-        {me && (
-          <div className="card">
-            <h3 className="align-center justify-center">My Dives</h3>
-            <MyDiveList logs={me.logs} />
-          </div>
-        )}
+  else if (filterState === 'All') {
+    return (
+      <div className="dashContainer container text-dark mb-4 py-3 flex-column align-center" style={{ height: "600px" }}>
+        <div className="container flex-column justify-space-between-lg justify-center align-center">
+          {me && (
+            <div className="card">
+              <h3 className="align-center justify-center">My Dives</h3>
+              <MyDiveList logs={sortedLogs} />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    )
+  } else {
+    return (
+      <div className="dashContainer container text-dark mb-4 py-3 flex-column align-center" style={{ height: "600px" }}>
+        <div className="container flex-column justify-space-between-lg justify-center align-center">
+          {me && (
+            <div className="card">
+              <h3 className="align-center justify-center">My Dives</h3>
+              <MyDiveList logs={me.logs} />
+            </div>
+          )}
+        </div>
+      </div>
+    )
+  };
 };
 
 export default MyDives;
